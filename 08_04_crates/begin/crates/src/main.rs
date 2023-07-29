8c004565559c0bea0d55186f6e5cffa192fefae7
PrimeTime416 <a2kerr@gmail.com>
2023-07-29 17:10:25 -0400

    completing leasons

--- 08_04_crates/begin/crates/src/main.rs
+++ 08_04_crates/begin/crates/src/main.rs
@@ -1,3 +1,13 @@
+// use core::num::dec2flt::number;
+
+use rand::prelude::*;
+
+
 fn main() {
+    let number = random::<f64>();
+    println!("Randum number is: {:?}", number);
+
+    let number = thread_rng().gen_range(1..11);
+    println!("Randum number is: {:?}", number);
     
 }
--- 09_02_read_files/begin/read_files/src/main.rs
+++ 09_02_read_files/begin/read_files/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use std::fs;
 
+fn main() {
+    let contents = fs::read_to_string("planets.txt").unwrap();
+    println!("The contents are:  {:}", contents);
+
+    for line in contents.lines(){
+        println!("Line is {}", line);
+    }
+
+    let contents = fs::read("planets.txt").unwrap();
+    println!("The contents are:  {:?}", contents)
 }
